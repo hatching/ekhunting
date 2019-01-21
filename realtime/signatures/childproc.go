fac8594f6a1d072a0349287bafd07eba28c1fcd9
Jurriaan Bremer <jbr@hatching.io>
2019-01-21 21:02:54 +0100

    use sig as class object varname

--- realtime/signatures/childproc.go
+++ realtime/signatures/childproc.go
@@ -4,8 +4,6 @@
 package signatures
 
 import (
-	"log"
-
 	"hatching.io/realtime/events/onemon"
 )
 
@@ -23,16 +21,16 @@ var whitelist = map[string]bool{
 	"\\??\\C:\\Windows\\System32\\ie4uinit.exe": true,
 }
 
-func (cp *ChildProcess) Init() {
-	cp.image = map[uint64]string{}
+func (sig *ChildProcess) Init() {
+	sig.image = map[uint64]string{}
 }
 
-func (cp *ChildProcess) Process(process *onemon.Process) {
+func (sig *ChildProcess) Process(process *onemon.Process) {
 	if process.Status == onemon.ProcessStatus_Terminate {
 		return
 	}
 
-	cp.image[process.Pid] = process.Image
+	sig.image[process.Pid] = process.Image
 
 	// Internet Explorer process.
 	if _, ok := iexplore[process.Image]; ok {
@@ -40,16 +38,16 @@ func (cp *ChildProcess) Process(process *onemon.Process) {
 	}
 
 	// Not a tracked process (ignore for now).
-	if _, ok := cp.image[process.Ppid]; !ok {
+	if _, ok := sig.image[process.Ppid]; !ok {
 		return
 	}
 
 	// Whitelisted child process of Internet Explorer.
-	if _, ok := iexplore[cp.image[process.Ppid]]; ok {
+	if _, ok := iexplore[sig.image[process.Ppid]]; ok {
 		if _, ok := whitelist[process.Image]; ok {
 			return
 		}
 	}
 
-	cp.Trigger("child_process", "A malicious process was started", process.Command)
+	sig.Trigger("child_process", "A malicious process was started", process.Command)
 }
